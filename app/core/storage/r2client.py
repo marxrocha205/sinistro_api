ab49f7d16648126cb36fdb36a098758af3ae53cb
Marx Rocha <mresportx@gmail.com>
2026-01-23 16:02:17 -0300

    fix: r2

--- app/core/storage/r2client.py
+++ app/core/storage/r2client.py
@@ -1,15 +1,15 @@
 import boto3
-import os
+from app.core.config import settings
 
 session = boto3.session.Session()
 
 s3 = session.client(
     service_name="s3",
-    endpoint_url=os.getenv("R2_ENDPOINT"),
-    aws_access_key_id=os.getenv("R2_ACCESS_KEY"),
-    aws_secret_access_key=os.getenv("R2_SECRET_KEY"),
+    endpoint_url=settings.r2_endpoint,
+    aws_access_key_id=settings.r2_access_key,
+    aws_secret_access_key=settings.r2_secret_key,
     region_name="auto",
 )
 
-BUCKET = os.getenv("R2_BUCKET")
-PUBLIC_URL = os.getenv("R2_PUBLIC_URL")
+BUCKET = settings.r2_bucket
+PUBLIC_URL = settings.r2_public_url
