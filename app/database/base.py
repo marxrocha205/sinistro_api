f19ce6de1df655189a5d9518c62dc1aad7f11ff1
Marx Rocha <165192193+marxrocha205@users.noreply.github.com>
2026-01-22 22:38:21 -0300

    Update base.py

--- app/database/base.py
+++ app/database/base.py
@@ -3,3 +3,8 @@ from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase):
     pass
+
+from app.models.veiculo import Veiculo
+from app.models.condutor import Condutor
+from app.models.user import User
+from app.models.sinistro import Sinistro
