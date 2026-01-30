f1ce8de6deba68509217290ccf09e1ac62a1363c
Marx Rocha <mresportx@gmail.com>
2026-01-30 09:06:46 -0300

    fix: importação do boolean

--- app/models/condutor.py
+++ app/models/condutor.py
@@ -1,5 +1,5 @@
-from xmlrpc.client import Boolean
-from sqlalchemy import Column, Integer, String, ForeignKey
+
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from app.database.base import Base
 
